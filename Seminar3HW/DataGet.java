12362fc3eec9c4f86bff84065548086ac4c754ea
Георгий Морозов <118921339+Karhashim84@users.noreply.github.com>
2024-02-20 19:47:39 +0500

    Дз семинара 3

---
+++ Seminar3HW/DataGet.java
@@ -0,0 +1,33 @@
+package Seminar3HW;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DataGet {
+    public static void getData() throws EmptyStringException, ArrayCountElementException, IncorrectValueException, IOException {
+
+       try (Scanner scanner = new Scanner(System.in)) {
+        System.out.println("Введите данные в порядке указанном ниже латиницей:\n" +
+                   " <Фамилия> <Имя> <Отчество> <Дата рождения> <Телефон> <Пол>\n " +
+                   "Пример:\n" +
+                   "Morozov Georgy Alexandrovich 05.04.1984 89555555555 f");
+           String inputData;
+           inputData = scanner.nextLine();
+           if (inputData.isEmpty()){
+               throw new EmptyStringException("Строка является пустой");
+           }
+           inputData = inputData.replaceAll("\\s+", " ");
+           inputData = inputData.trim();
+           String[] bufferArray = inputData.split(" ");
+           if (bufferArray.length == 6){
+               Check.checkDateFormat(bufferArray[3]);
+               Check.checkPhoneNumberFormat(bufferArray[4]);
+               Check.checkGenderFormat(bufferArray[5]);
+               DataWrite.createFile(bufferArray);
+            } else {
+               throw new ArrayCountElementException("Вы ввели не все данные или\n" +
+                       "Вы пропустили пробел");
+           }
+        }
+    }
+}
---
+++ Seminar3HW/IncorrectValueException.java
@@ -0,0 +1,18 @@
+package Seminar3HW;
+
+
+class IncorrectValueException extends RuntimeException{
+    public IncorrectValueException(String message) {
+        super(message);
+    }}
+class EmptyStringException extends RuntimeException{
+    public EmptyStringException(String message) {
+        super(message);
+    }
+}
+
+class ArrayCountElementException extends RuntimeException{
+    public ArrayCountElementException(String message) {
+        super(message);
+    }
+}
